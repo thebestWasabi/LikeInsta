61df3208bcd1c91e4eb9650efba2e4b6677fc45f
thebestWasabi <openpw@yandex.ru>
2023-07-16 21:32:08 +0300

    Загрузил начальную конфигурацию

---
+++ src/main/java/io/khamzin/likeinsta/LikeInstaApplication.java
@@ -0,0 +1,13 @@
+package io.khamzin.likeinsta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LikeInstaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LikeInstaApplication.class, args);
+    }
+
+}
