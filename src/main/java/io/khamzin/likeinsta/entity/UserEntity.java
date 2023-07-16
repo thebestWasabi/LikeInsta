b271bb1bb01c7a8c361b0ad150e2c1baf458f635
thebestWasabi <openpw@yandex.ru>
2023-07-17 01:53:24 +0300

    Загрузил CustomUserDetailsService
    Загрузил CustomUserDetailsService, а так же добавил имплементацию UserDetails в UserEntity

--- src/main/java/io/khamzin/likeinsta/entity/UserEntity.java
+++ src/main/java/io/khamzin/likeinsta/entity/UserEntity.java
@@ -2,20 +2,31 @@ package io.khamzin.likeinsta.entity;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import io.khamzin.likeinsta.entity.enums.ERole;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.*;
 
+import static javax.persistence.CascadeType.ALL;
+import static javax.persistence.FetchType.LAZY;
+import static javax.persistence.GenerationType.IDENTITY;
+
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "user")
-public class UserEntity {
+public class UserEntity implements UserDetails {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = IDENTITY)
     private Long id;
 
     @Column(nullable = false)
@@ -40,7 +51,7 @@ public class UserEntity {
     @CollectionTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"))
     private Set<ERole> roles = new HashSet<>();
 
-    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "user", orphanRemoval = true)
+    @OneToMany(cascade = ALL, fetch = LAZY, mappedBy = "user", orphanRemoval = true)
     private List<PostEntity> posts = new ArrayList<>();
 
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
@@ -53,6 +64,37 @@ public class UserEntity {
     @PrePersist
     protected void onCreate() {
         createdDate = LocalDateTime.now();
+    }
+
+
+    /**
+     * Security
+     */
+
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 
 }
