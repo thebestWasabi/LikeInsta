192cfc0d78695f80453ac101190a410631780f14
thebestWasabi <openpw@yandex.ru>
2023-07-17 19:38:07 +0300

    Загрузил EmailValidator и ValidEmail

---
+++ src/main/java/io/khamzin/likeinsta/annotation/ValidEmail.java
@@ -0,0 +1,25 @@
+package io.khamzin.likeinsta.annotation;
+
+import io.khamzin.likeinsta.validations.EmailValidator;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.*;
+
+@Target({TYPE, FIELD, ANNOTATION_TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = EmailValidator.class)
+@Documented
+public @interface ValidEmail {
+
+    String message() default "Invalid email";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}
