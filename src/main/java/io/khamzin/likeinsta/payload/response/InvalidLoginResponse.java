488ab1e688ba6e4973f87fb71020c1a5be77718b
thebestWasabi <openpw@yandex.ru>
2023-07-17 17:43:10 +0300

    Загрузил InvalidLoginResponse
    Добавил класс для создания объектов-ошибок

---
+++ src/main/java/io/khamzin/likeinsta/payload/response/InvalidLoginResponse.java
@@ -0,0 +1,16 @@
+package io.khamzin.likeinsta.payload.response;
+
+import lombok.Getter;
+
+@Getter
+public class InvalidLoginResponse {
+
+    private String username;
+    private String password;
+
+    public InvalidLoginResponse() {
+        this.username = "Invalid username";
+        this.password = "Invalid password";
+    }
+
+}
