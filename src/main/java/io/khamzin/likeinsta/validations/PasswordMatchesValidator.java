e08b61f2b7683d6bb636d33fdc2bd0d0b4e5d6f9
thebestWasabi <openpw@yandex.ru>
2023-07-18 01:13:57 +0300

    Загрузил MessageResponse и JWTTokenSuccessResponse
    Так же подчистил EmailValidator и PasswordMatchesValidator

--- src/main/java/io/khamzin/likeinsta/validations/PasswordMatchesValidator.java
+++ src/main/java/io/khamzin/likeinsta/validations/PasswordMatchesValidator.java
@@ -9,11 +9,6 @@ import javax.validation.ConstraintValidatorContext;
 public class PasswordMatchesValidator implements ConstraintValidator<PasswordMatches, Object> {
 
     @Override
-    public void initialize(PasswordMatches constraintAnnotation) {
-//        ConstraintValidator.super.initialize(constraintAnnotation);
-    }
-
-    @Override
     public boolean isValid(Object object, ConstraintValidatorContext constraintValidatorContext) {
         SignupRequest userSignupRequest = (SignupRequest) object;
         return userSignupRequest.getPassword().equals(userSignupRequest.getConfirmPassword());
