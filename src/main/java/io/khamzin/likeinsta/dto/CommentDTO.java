29519bd45779924e95fc66101886da8ac33037b0
thebestWasabi <openpw@yandex.ru>
2023-07-20 05:42:27 +0300

    Дополнил классы UserDTO, PostDTO и CommentDTO

--- src/main/java/io/khamzin/likeinsta/dto/CommentDTO.java
+++ src/main/java/io/khamzin/likeinsta/dto/CommentDTO.java
@@ -1,14 +1,17 @@
 package io.khamzin.likeinsta.dto;
 
+import lombok.Builder;
 import lombok.Data;
 
 import javax.validation.constraints.NotBlank;
 
 @Data
+@Builder
 public class CommentDTO {
 
     private Long id;
     @NotBlank
     private String message;
     private String username;
+
 }
--- src/main/java/io/khamzin/likeinsta/dto/PostDTO.java
+++ src/main/java/io/khamzin/likeinsta/dto/PostDTO.java
@@ -1,10 +1,12 @@
 package io.khamzin.likeinsta.dto;
 
+import lombok.Builder;
 import lombok.Data;
 
 import java.util.Set;
 
 @Data
+@Builder
 public class PostDTO {
 
     private Long id;
